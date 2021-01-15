5e805eb1553ade0f9d5637d926662663f2030278
bitmage <q@bitmage.net>
2021-01-15 14:08:24 -0700

    this experiment doesn't compile

--- impls/rad/reader.rs
+++ impls/rad/reader.rs
@@ -2,7 +2,7 @@ use lazy_static::lazy_static;
 use regex::Regex;
 use std::iter::Peekable;
 
-use types::{RadNode, RadList, RadLink, RadType};
+use types::{RadNode, RadList, RadType};
 
 #[cfg(test)]
 mod test {
@@ -37,62 +37,39 @@ fn tokenize(input: &str) -> Tokens {
     matches
 }
 
-pub fn read_str(input: &str) -> Option<RadNode> {
+pub fn read_str(input: &str) -> RadList {
     let tokens = tokenize(input);
-    let (list, _) = read_form(&tokens, 0);
-    list
+    read_form(&tokens)
 }
 
-fn read_form(tokens: &Tokens, pos: usize) -> (Option<RadNode>, usize) {
-    let token = &tokens.get(pos);
+fn read_form(tokens: &Tokens) -> RadList {
+    let reader = tokens.iter().peekable();
+    let token = reader.peek();
     match token.map(|t| t.as_str()) {
-        Some("(") => read_list(tokens, pos),
-        Some(_) => read_atom(tokens, pos),
-        None => (None, pos)
+        Some("(") => read_list(reader),
+        Some(_) => read_atom(reader),
+        None => RadList::new()
     }
 }
 
-fn read_list(tokens: &Tokens, mut pos: usize) -> (Option<RadNode>, usize)
-{
-    let mut args = RadList::new();
-    //let src = Vec<String>::new();
-
-    let mut _t: Option<&str> = None;
-    loop {
-
-        // get the next token
-        pos += 1;
-        _t = tokens.get(pos).map(|s| s.as_str());
-
-        match _t {
-            // end the list
-            Some(")") => break,
-            None => panic!("Unterminated list!"),
-
-            // process a list item
-            Some(_) => {
-                let (form, _pos) = read_form(tokens, pos);
-                pos = _pos;
-                args.push(form.unwrap());
-            },
-        }
-    }
-    let node = RadNode {
-        src: "".to_string(),
-        rtype: RadType::List,
-        args: RadList::new(),
-    };
-    (Some(node), pos)
-}
-
-fn read_atom(tokens: &Tokens, pos: usize) -> (Option<RadNode>, usize)
+fn read_list<I: AsRef<str>>(reader: impl Iterator<Item = I>) -> RadList
 {
     // this should be safe because we peek before getting here
-    let src = tokens.get(pos).unwrap().to_string();
-    let node = RadNode {
-        src: src,
+    let src = reader.next().unwrap();
+    vec!(RadNode {
+        src: src.as_ref().to_string(),
+        rtype: RadType::List,
+        args: RadList::new(),
+    })
+}
+
+fn read_atom<I: AsRef<str>>(reader: impl Iterator<Item = I>) -> RadList
+{
+    // this should be safe because we peek before getting here
+    let src = reader.next().unwrap();
+    vec!(RadNode {
+        src: src.as_ref().to_string(),
         rtype: RadType::RString,
         args: RadList::new(),
-    };
-    (Some(node), pos + 1)
+    })
 }
